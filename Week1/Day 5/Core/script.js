a51e39395d0515ec58e6aa4dcda5bf9c80995386
Anisghadhab <anisghadhab@utctunisie.com>
2023-05-08 20:23:53 +0100

    Button Clicker

---
+++ Week1/Day 5/Core/script.js
@@ -0,0 +1,10 @@
+function changeButton(element){
+    element.innerText = "Logout";
+    console.log('Hello!');
+}
+function removeButton(element) {
+    element.remove();
+}
+function ninjAlert(element) {
+    alert('This page says Ninja was liked');
+}
