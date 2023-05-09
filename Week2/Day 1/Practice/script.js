e35ca12058b7b0a13de09edb47d73d6e74926e1b
Anisghadhab <anisghadhab@utctunisie.com>
2023-05-09 20:59:04 +0100

    Video Preview

---
+++ Week2/Day 1/Practice/script.js
@@ -0,0 +1,18 @@
+
+function playVideo(element){
+    element.play();
+
+
+}
+function pauseVideo(element) {
+    element.pause();
+}
+function muteVideo(element) {
+    element.Mute
+}
+var shut = document.querySelector("VIDEO");
+function muteVideo(element){
+    element.mute = true;
+
+}
+muteVideo(shut)
