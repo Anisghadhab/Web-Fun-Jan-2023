aed4ef90ac3b1967e3115a9ac57e9a1078a4b485
Anisghadhab <anisghadhab@utctunisie.com>
2023-05-10 14:59:31 +0100

    Liking likes

---
+++ Week2/Day 1/Core/Likes/script.js
@@ -0,0 +1,19 @@
+var x = 1;
+var Like1 = document.querySelector("#like1")
+function add1() {
+    
+    Like1.innerText = x + 9 + "like(s)";
+    x++;
+}
+var y = 1;
+var Like2 = document.querySelector("#like2")
+function add2() {
+    Like2.innerText = y + 12 + "likes(s)"
+    y++;
+}
+var z = 1;
+var Like3 = document.querySelector("#like3")
+function add3() {
+    Like3.innerText = z + 9 + "likes(s)"
+    z++;
+}
