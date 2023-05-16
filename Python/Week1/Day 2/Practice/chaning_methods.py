eaed452f3a21b56d64d1f08efdca2dbba1de74a1
Anisghadhab <anisghadhab@utctunisie.com>
2023-05-16 15:29:10 +0100

    pratice

--- Python/Week1/Day 2/Practice/chaning_methods.py
+++ Python/Week1/Day 2/Practice/chaning_methods.py
@@ -18,12 +18,13 @@ class User:
     def enroll(self):
         self.is_rewards_member = True
         self.gold_card_points = 200
-    
+        return self
     def spend_points(self, amount):
         if amount < self.gold_card_points:
             self.gold_card_points -= amount
         else:
             print("not enough points")
+        return self
 
 Anis_user = User("Anis", "Ghadhab", "anisghadhab@utctunisie.com", 30)
 
@@ -31,18 +32,9 @@ Aymen_user = User("Aymen", "Ghadhab", "akay442@hotmail.com", 33)
 
 Atef_user = User("Atef", "Bhouri", "atefbhouri@hotmail.com", 55)
 
-Anis_user.display_info()
-Anis_user.enroll()
-Anis_user.spend_points(50)
-Aymen_user.enroll()
-Aymen_user.spend_points(80)
+Anis_user.enroll().spend_points(50).display_info()
 
-Anis_user.display_info()
-Aymen_user.display_info()
-Atef_user.display_info()
+Aymen_user.enroll().spend_points(80).display_info()
 
-Atef_user.spend_points(40)
-
-
-
+Atef_user.spend_points(40).display_info()
 
