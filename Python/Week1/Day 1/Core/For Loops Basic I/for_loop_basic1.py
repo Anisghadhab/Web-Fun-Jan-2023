776906c288e562835dff8f76988035451f228ec2
Anisghadhab <anisghadhab@utctunisie.com>
2023-05-16 14:51:46 +0100

    Nested

---
+++ Python/Week1/Day 1/Core/For Loops Basic I/for_loop_basic1.py
@@ -0,0 +1,27 @@
+for x in range(0, 151):
+    print(x)
+
+for x in range(5, 1001):
+    if x % 5 == 0:
+        print(x)
+
+for x in range(0, 101):
+    if x % 5 == 0:
+        print("Coding")
+    if x % 10 == 0:
+        print("Coding Dojo")
+sum = 0
+for x in range(0, 500001):
+    if x % 2 != 0:
+        sum = x + sum
+print(sum)
+
+for x in range (2018,-1,-4):
+    if x > 0:
+        print(x)
+count = []
+for x in range (0,420,3):
+    if x % 3 == 0:
+        count.append(x)
+print(count)
+
