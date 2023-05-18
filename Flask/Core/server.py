ba123a734a85fa1657f4499ef0ac640155a7f2cd
Anisghadhab <anisghadhab@utctunisie.com>
2023-05-18 16:40:53 +0100

    Playground

---
+++ Flask/Core/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/play/<int:num>/<string:color>")
+def hello_world(num, color):
+    return render_template("index.html", num=num, color=color)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
